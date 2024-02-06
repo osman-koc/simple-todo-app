4635d25d56543e8bc47c8df4687abb75da448986
Osman Koç <kocosm@hotmail.com>
2024-02-06 22:30:39 +0300

    chore: update last sizedbox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -135,7 +135,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-        const SizedBox(height: 100)
+        const SizedBox(height: 50)
       ]),
     );
   }
